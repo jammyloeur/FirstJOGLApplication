64fb7d3ba320f385c2ef8f0959c894c89f3be6dc
Jammy Loeur <jammyloeur@csus.edu>
2018-08-31 10:10:52 -0700

    Add animated triangle.

--- src/Code.java
+++ src/Code.java
@@ -1,7 +1,11 @@
+import java.nio.*;
 import javax.swing.*;
 import static com.jogamp.opengl.GL4.*;
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.GLContext;
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.util.*;
 
 import graphicslib3D.*;
 import graphicslib3D.GLSLUtils.*;
@@ -11,6 +15,8 @@ public class Code extends JFrame implements GLEventListener {
     private int vao[] = new int[1];
     private GLCanvas myCanvas;
     private GLSLUtils util = new GLSLUtils();
+    private float x = 0.0f; // location of triangle
+    private float inc = 0.01f; // offset for moving the triangle
 
     public Code() {
         setTitle("Chapter2 - program1");
@@ -20,13 +26,22 @@ public class Code extends JFrame implements GLEventListener {
         myCanvas.addGLEventListener(this);
         this.add(myCanvas);
         setVisible(true);
+        FPSAnimator animator = new FPSAnimator(myCanvas, 50);
+        animator.start();
     }
 
     public void display(GLAutoDrawable drawable) {
         GL4 gl = (GL4) GLContext.getCurrentGL();
         gl.glUseProgram(rendering_program);
-        gl.glPointSize(30.0f);
-        gl.glDrawArrays(GL_POINTS, 0, 1);
+        float bkg[] = { 0.0f, 0.0f, 0.0f, 1.0f }; // clear the background to black, each time
+        FloatBuffer bkgBuffer = Buffers.newDirectFloatBuffer(bkg);
+        gl.glClearBufferfv(GL_COLOR, 0, bkgBuffer);
+        x += inc;
+        if (x > 1.0f) inc = -0.01f;
+        if (x < -1.0f) inc = 0.01f;
+        int offset_loc = gl.glGetUniformLocation(rendering_program, "offset");
+        gl.glProgramUniform1f(rendering_program, offset_loc, x);
+        gl.glDrawArrays(GL_TRIANGLES, 0, 3);
     }
 
     public static void main(String[ ] args) {
